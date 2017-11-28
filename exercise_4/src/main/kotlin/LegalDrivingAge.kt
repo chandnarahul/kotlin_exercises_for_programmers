a62e2729542fd328209ffd03b966356cb70299da
rahul.chandna <chandnarahul@yahoo.com>
2017-11-28 21:36:39 +0200

    added driving code

--- exercise_4/src/main/kotlin/LegalDrivingAge.kt
+++ exercise_4/src/main/kotlin/LegalDrivingAge.kt
@@ -1,5 +1,27 @@
 fun main(args: Array<String>) {
-    print("What is your age?")
-    var age = readLine().toString().toInt();
-    println(if (age <= 15) "You are not old enough to legally drive." else "You are old enough to legally drive.")
+    var run = true;
+    var age = 0;
+    var map: MutableMap<String, Int> = HashMap();
+    map.put("IN", 18)
+    map.put("FK", 16)
+    map.put("RC", 12)
+    map.put("BC", 10)
+    while (run) {
+        try {
+            print("What is your age?")
+            age = readLine().toString().toInt();
+            if (age < 0) {
+                throw Exception("");
+            } else {
+                break;
+            }
+        } catch (e: Exception) {
+            println("enter a valid age.");
+        }
+    }
+    for (entry in map) {
+        if (entry.value <= age) {
+            println("You can legally drive in ${entry.key}");
+        }
+    }
 }
