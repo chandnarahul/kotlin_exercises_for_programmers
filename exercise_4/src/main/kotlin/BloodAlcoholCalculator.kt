a606dc637bbb06dd7835aa106de7ebfc3c4941dd
rahul.chandna <chandnarahul@yahoo.com>
2017-11-28 22:21:28 +0200

    added metric system

--- exercise_4/src/main/kotlin/BloodAlcoholCalculator.kt
+++ exercise_4/src/main/kotlin/BloodAlcoholCalculator.kt
@@ -4,15 +4,33 @@ fun main(args: Array<String>) {
     map.put("FK", 0.04)
     map.put("RC", 0.05)
     map.put("BC", 0.06)
+    pound(map)
+    kilogram(map)
+}
+
+fun kilogram(map: MutableMap<String, Double>) {
+    val a = userInput("total alcohol consumed, in grams.")
+    val w = userInput("body weight in kilogram.")
+    val h = userInput("number of hours since the last drink.")
+    var r = getR()
+    val bac = ((a * 28.35) * 5.14 / (w * 0.453592) * r) - .015 * h;
+    println("Your BAC is ${Math.round(bac * 100) / 100.0}")
+
+    for (entry in map) {
+        if (bac <= entry.value) {
+            println("You can legally drive in ${entry.key}");
+        }
+    }
+    if (bac >= 0.08) {
+        println("It is not legal for you to drive.")
+    }
+}
+
+private fun pound(map: MutableMap<String, Double>) {
     val a = userInput("total alcohol consumed, in ounces (oz)")
     val w = userInput("body weight in pounds.")
     val h = userInput("number of hours since the last drink.")
-    print("gender")
-    val g = readLine().toString()
-    var r = 0.73;
-    if (g.equals("f", true)) {
-        r = 0.66;
-    }
+    var r = getR()
     val bac = (a * 5.14 / w * r) - .015 * h;
     println("Your BAC is ${Math.round(bac * 100) / 100.0}")
 
@@ -24,6 +42,16 @@ fun main(args: Array<String>) {
     if (bac >= 0.08) {
         println("It is not legal for you to drive.")
     }
+}
+
+private fun getR(): Double {
+    print("gender")
+    val g = readLine().toString()
+    var r = 0.73;
+    if (g.equals("f", true)) {
+        r = 0.66;
+    }
+    return r
 }
 
 private fun userInput(msg: String): Double {
